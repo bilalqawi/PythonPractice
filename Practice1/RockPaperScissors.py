c522d77bd3f3c6db27546ae1df9ff292db0ae6e3
Bilal Qawi <princetayer@gmail.com>
2025-01-19 18:45:15 -0600

    Added comments

--- Practice1/RockPaperScissors.py
+++ Practice1/RockPaperScissors.py
@@ -1,7 +1,9 @@
 import random;
-
+# Print statement introducing the Rock Paper Scissors Game
 print("Welcome to the Rock, Paper and Scissors Game");
 
+# get_choices function will get the user input and store it as a choice
+# this function will also use random library to randomize computer choice option
 def get_choices():
     player_choice = input("Enter a choice (Rock, Paper, Scissors): ");
 
@@ -15,7 +17,7 @@ def get_choices():
     choices = {"Player": player_choice, "Computer": computer_choice};
     return choices;
 
-
+# declare_win function will filter through choices and declare whether the player won or lost
 def declare_win(player, computer):
     if(player == computer): 
         return "Player and Computer Tied";
@@ -26,6 +28,7 @@ def declare_win(player, computer):
     else: 
         return "Player Won";
 
+# Here is where the functions are called and finally the results are printed.
 choices = get_choices();
 print("Computer picked: " + choices["Computer"]);
 win_status = declare_win(choices["Player"], choices["Computer"]);
