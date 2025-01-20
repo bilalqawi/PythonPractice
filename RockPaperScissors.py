25978798c60f1cefa6b88b3c2e26d2df0c1efe65
Bilal Qawi <princetayer@gmail.com>
2025-01-19 18:19:47 -0600

    Added Rock Paper Scissors Game as Practice file 1
    Practice 1 in Python, added Rock Paper Scissors

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -1,5 +1,32 @@
-def get_choices()
-    player_choice = "Rock";
-    computer_choice = "Paper";
+import random;
 
-    return player_choice;
+print("Welcome to the Rock, Paper and Scissors Game");
+
+def get_choices():
+    player_choice = input("Enter a choice (Rock, Paper, Scissors): ");
+
+    while player_choice not in ["Rock", "Paper", "Scissors"]:
+        print("Invalid option selected, please try again");
+        player_choice = input("Enter a choice (Rock, Paper, Scissors): ");
+
+    print("Player selected " + player_choice);
+    options = ["Rock", "Paper", "Scissors"];
+    computer_choice = random.choice(options);
+    choices = {"Player": player_choice, "Computer": computer_choice};
+    return choices;
+
+
+def declare_win(player, computer):
+    if(player == computer): 
+        return "Player and Computer Tied";
+    elif ((player == "Rock" and computer == "Paper") or
+      (player == "Scissors" and computer == "Rock") or
+      (player == "Paper" and computer == "Scissors")):
+        return "Player Lost";
+    else: 
+        return "Player Won";
+
+choices = get_choices();
+print("Computer picked: " + choices["Computer"]);
+win_status = declare_win(choices["Player"], choices["Computer"]);
+print(win_status);
